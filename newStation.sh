5b12d1a17482a84aab36b0bfbe82e7b47dd7d649
nilsonrochabh <nilson.f.rocha@gmail.com>
2024-04-02 07:48:45 -0300

    files

--- newStation.sh
+++ newStation.sh
@@ -9,15 +9,15 @@ read station
 echo $station.aco-connexion.org
 
 ##criando e editando o arquivo station
-cp file.aco-connexion.org $station.aco-connexion.org
+cp stations/file.aco-connexion.org $station.aco-connexion.org
 sed -i 's/file/'$station'/g' $station.aco-connexion.org
 
 #conf/transport.BRA31LA
 sed -i -e '$a'$station'.aco-connexion.org uucp:'$station'' transport.BRA31LA
 
 #editando o /etc/bind/zones/aco-connexion.org
-sed -i -e '50i\' -e ''$station'.aco-connexion.org        IN      A       104.248.253.98' aco-connexion.org
-sed -i -e '80i\' -e ''$station'         TXT  "v=spf1 ip4:104.248.253.98 -all"' aco-connexion.org 
+sed -i -e '50i\' -e ''$station'.aco-connexion.org        IN      A       104.248.253.98' zones/aco-connexion.org
+sed -i -e '80i\' -e ''$station'         TXT  "v=spf1 ip4:104.248.253.98 -all"' zones/aco-connexion.org 
 
 ##Criando a estação VPN
 pivpn -a -d 3650 nopass -n $station.aco.connexion.org
